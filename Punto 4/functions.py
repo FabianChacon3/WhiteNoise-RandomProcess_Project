ebcbe83fec9638b9c16b278efd82cc64f251236e
GOST1706 <jbugra23@outlook.es>
2025-04-25 14:35:37 -0500

    Se agregan comentarios

--- Punto 4/functions.py
+++ Punto 4/functions.py
@@ -4,6 +4,12 @@ from scipy.fft import fft, ifft, fftfreq, fftshift
 
 # Genera una señal de ruido blanco gaussiano (media 0, varianza 1)
 def gaussian_noise(t, A):
+    # Entrada: 
+    #   t : vector de tiempo (define la longitud del ruido)
+    #   A : tamaño de la ventana para la convolución
+    # Salida: 
+    #   x_gauss : señal de ruido blanco suavizado por convolución
+
     x = np.random.normal(0, 1, len(t))
     ventana = np.ones(A) / A
     x_gauss = lfilter(ventana, [1], x)      # Convolución recursiva
@@ -11,6 +17,17 @@ def gaussian_noise(t, A):
 
 # Modulador QAM modo AM
 def qam_modulator(message, Tbit=0.001, A=1, ka=0.5, fc=1e9, fs=5e9):
+    # Entrada:
+    #   message : señal digital (lista o array de bits)
+    #   Tbit : duración de un bit (en segundos)
+    #   A : amplitud de la portadora
+    #   ka : índice de modulación
+    #   fc : frecuencia de la portadora (Hz)
+    #   fs : frecuencia de muestreo (Hz)
+    # Salida:
+    #   t : vector de tiempo correspondiente a la señal modulada
+    #   s_t : señal modulada en AM
+
     Ns = int(fs * Tbit)                    # Muestras por bit
     total_samples = Ns * len(message)      # Total de muestras
     t = np.arange(0, total_samples) / fs   # Vector de tiempo
@@ -23,14 +40,24 @@ def qam_modulator(message, Tbit=0.001, A=1, ka=0.5, fc=1e9, fs=5e9):
 
     return t, s_t
 
-# Simula la atenuación y dispersion en una fibra optica monomodo
+# Modelo de canal de una fibra optica monomodo
 def fibra_optica(signal, t, D=17, L=0.1, atenuation=0.2, fc=1e9):
+    # Entrada:
+    #   signal : señal modulada a transmitir
+    #   t : vector de tiempo de la señal
+    #   D : parámetro de dispersión cromática (ps/nm/km)
+    #   L : longitud de la fibra (km)
+    #   atenuation : pérdida en dB/km
+    #   fc : frecuencia de la portadora (Hz)
+    # Salida:
+    #   signal_out : señal recibida después de la propagación
+
     # Constantes
     c = 3e8  # velocidad de la luz (m/s)
     lambda_m = c / fc  # longitud de onda en metros a partir de fc
 
     # Convertir parámetros
-    alpha_np = atenuation / 4.343  # Np/km
+    alpha_np = atenuation / 4.343  # dB/km --> Np/km
 
     # Coeficiente de propagación
     beta2 = (D * 1e-6) * (lambda_m**2) / (2 * np.pi * c)  # s^2/m
@@ -57,6 +84,14 @@ def fibra_optica(signal, t, D=17, L=0.1, atenuation=0.2, fc=1e9):
 
 # Filtro de dispersion
 def disperfilter(signal, t, D=17, L=0.1,  fc=1e9):
+    # Entrada:
+    #   signal : señal afectada por la dispersión
+    #   t : vector de tiempo de la señal
+    #   D : parámetro de dispersión cromática (ps/nm/km)
+    #   L : longitud de la fibra (km)
+    #   fc : frecuencia de la portadora (Hz)
+    # Salida:
+    #   signal_out : señal corregida tras aplicar el filtro de dispersión
 
     c = 3e8                    # velocidad de la luz en m/s
     lambda_c = c / fc          # longitud de onda en metros
@@ -83,6 +118,13 @@ def disperfilter(signal, t, D=17, L=0.1,  fc=1e9):
 
 # Filtro de frecuencia 
 def freqfilter(signal, fs=5e9, fc=1e9, bw=2e4):
+    # Entrada:
+    #   signal : señal a filtrar
+    #   fs : frecuencia de muestreo (Hz)
+    #   fc : frecuencia central del filtro (Hz)
+    #   bw : ancho de banda del filtro (Hz)
+    # Salida:
+    #   signal_filtrada : señal tras aplicar el filtro en frecuencia
 
     N = len(signal)
     dt = 1 / fs
